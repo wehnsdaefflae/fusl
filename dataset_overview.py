060263ad16465fec851d9dbd18ae4a21a9d4652f
Mark <wernsdorfer@gmail.com>
2023-12-19 13:50:55 +0100

    gesamtüberblick

--- dataset_overview.py
+++ dataset_overview.py
@@ -4,6 +4,12 @@ from pathlib import Path
 
 import numpy
 import pandas
+import seaborn
+from matplotlib.colors import LinearSegmentedColormap
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.font_manager
+
+from matplotlib import pyplot
 
 numpy.seterr(all='raise')
 
@@ -28,7 +34,7 @@ def print_values(data: dict[str, float]) -> None:
     print(f"maximum\t{data['maximum']:.5f}")
 
 
-def main() -> None:
+def dataset_info() -> None:
     input_path = Path("data/")
     output_path = Path("output/")
 
@@ -49,14 +55,6 @@ def main() -> None:
         all_clay = list()
         all_silt = list()
         all_sand = list()
-
-        combination_str = f"data-{data}"
-
-        subset_path = output_path / combination_str
-        subset_path.mkdir(parents=True, exist_ok=True)
-
-        plot_subset_path = subset_path / "plots"
-        plot_subset_path.mkdir(parents=True, exist_ok=True)
 
         for row_index, (n, row) in enumerate(overview_sheet.iterrows()):
             row_index = int(row_index)
@@ -135,22 +133,132 @@ def main() -> None:
         print_values(sand_values)
 
 
-if __name__ == "__main__":
-    #  datenbank kennzahlen
-    #    für
-    #      lambda (wärmeleitfähigkeit)
-    #      theta (wassergehalt, g/cm^3)
-    #      rho (bulk density)
-    #      sättigung
-    #      clay
-    #      silt
-    #      sand
-    #    kennzahlen:
-    #      minimum
-    #      1. quantil
-    #      median
-    #      mean
-    #      3. quantil
-    #      maximum
+def dataset_plots() -> None:
+    pyplot.rcParams["font.family"] = "Palatino Linotype"
 
+    input_path = Path("data/")
+    output_path = Path("output_plots/")
+    output_path.mkdir(exist_ok=True)
+
+    measurements_input_file = input_path / "Messdatenbank_FAU_Stand_2023-11-08.xlsx"
+
+    data_measurement_sheets = pandas.read_excel(measurements_input_file, sheet_name=None)
+    overview_sheet = data_measurement_sheets.get("Übersicht")
+    data_density = "low", "high"
+    # data_density = "all",
+
+    particle_density = 2_650
+
+    font_manager = matplotlib.font_manager.fontManager
+    font_manager.addfont("/home/mark/Downloads/Palatino Linotype.ttf")
+
+    # seaborn.set(font="Palatino Linotype")
+
+    fig_sand = pyplot.figure(figsize=(8, 8))
+    ax_sand = fig_sand.gca()
+    ax_sand.set(
+        title="sand",
+        xlabel="volumetric water content θ [cm3/cm3]",
+        ylabel="thermal conductivity λ [W/(m∙K)]"
+    )
+
+    fig_no_sand = pyplot.figure(figsize=(8, 8))
+    ax_no_sand = fig_no_sand.gca()
+    ax_no_sand.set(
+        title="no sand",
+        xlabel="volumetric water content θ [cm3/cm3]",
+        ylabel="thermal conductivity λ [W/(m∙K)]"
+    )
+
+    for data in data_density:
+        all_lambda_sand = list()
+        all_theta_sand = list()
+
+        all_lambda_no_sand = list()
+        all_theta_no_sand = list()
+
+        for row_index, (n, row) in enumerate(overview_sheet.iterrows()):
+            print(f"{data} {row_index}")
+
+            sheet_index = row.values[0]
+            percentage_sand = row.values[2]
+            percentage_silt = row.values[3]
+            percentage_clay = row.values[4]
+            density_soil_non_si = row.values[5]
+
+            each_density = row.values[9]
+
+            if data == "low" and each_density != "low":
+                continue
+            if data == "high" and each_density != "high":
+                continue
+
+            each_sheet = data_measurement_sheets.get(f"{sheet_index}")
+            theta_array = each_sheet["θ [cm3/cm3]"].to_numpy()
+
+            lambda_array = each_sheet["λ [W/(m∙K)]"].to_numpy()
+            filter_array = (
+                    numpy.isfinite(lambda_array)
+                    & (0 < theta_array)
+            )
+
+            density_soil = density_soil_non_si * 1_000.  # g/cm3 -> kg/m3
+            porosity_ratio = 1. - density_soil / particle_density
+
+            theta_measurement_volumetric = theta_array[filter_array]
+            data_measured = lambda_array[filter_array]
+            steps = theta_measurement_volumetric / porosity_ratio
+            steps_max = numpy.max(steps)
+            if 1. < steps_max:
+                steps /= steps_max
+
+            # plot lambda against theta
+            if 50. < percentage_sand:
+                ax = fig_sand.gca()
+                color = "sienna"
+                all_lambda = all_lambda_sand
+                all_theta = all_theta_sand
+            else:
+                ax = fig_no_sand.gca()
+                color = "darkred"
+                all_lambda = all_lambda_no_sand
+                all_theta = all_theta_no_sand
+
+            if data == "low":
+                seaborn.lineplot(
+                    x=theta_measurement_volumetric, y=data_measured, color=color, alpha=1., ax=ax, size=1.,
+                    legend=False
+                )
+            elif data == "high":
+                all_lambda.extend(data_measured)
+                all_theta.extend(theta_measurement_volumetric)
+            else:
+                pass
+
+    cmap_sand = LinearSegmentedColormap.from_list("", [(1, 1, 1, 0), (244/255, 164/255, 96/255, 1)])  # RGBA for sandybrown
+    cmap_no_sand = LinearSegmentedColormap.from_list("", [(1, 1, 1, 0), (139/255, 0, 0, 1)])  # RGBA for darkred
+
+    seaborn.kdeplot(
+        x=all_theta_sand, y=all_lambda_sand, fill=True, cmap=cmap_sand, alpha=1., bw_adjust=1, ax=ax_sand,
+        legend=False
+    )
+    seaborn.kdeplot(
+        x=all_theta_no_sand, y=all_lambda_no_sand, fill=True, cmap=cmap_no_sand, alpha=1., bw_adjust=1, ax=ax_no_sand,
+        legend=False
+    )
+
+    fig_sand.savefig(output_path / f"sand.png")
+    fig_no_sand.savefig(output_path / f"no_sand.png")
+
+    # fig.show()
+    pyplot.show()
+    # pyplot.clf()
+
+
+def main():
+    # dataset_info()
+    dataset_plots()
+
+
+if __name__ == "__main__":
     main()
